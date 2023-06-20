d7133869e8d5857759e9dedad147515256b572bf
Matjaž Finžgar <matjaz@finzgar.net>
2023-06-20 16:28:07 +0200

    fix: some more improvements for restore

--- apps/postgres-minio-restore/entrypoint.sh
+++ apps/postgres-minio-restore/entrypoint.sh
@@ -31,6 +31,6 @@ mc alias set s3 $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 
 mc cp s3/$MINIO_BUCKET/$DUMP_FILE /tmp/
 
-psql --command "drop owned by $INIT_POSTGRES_USER" --dbname $INIT_POSTGRES_DBNAME
+# psql --command "drop owned by $INIT_POSTGRES_USER" --dbname $INIT_POSTGRES_DBNAME
 
-pg_restore -Fc -j 8 -d $INIT_POSTGRES_DBNAME /tmp/$DUMP_FILE
+pg_restore -Fc -j 8 --clean --if-exists --verbose -d $INIT_POSTGRES_DBNAME /tmp/$DUMP_FILE
